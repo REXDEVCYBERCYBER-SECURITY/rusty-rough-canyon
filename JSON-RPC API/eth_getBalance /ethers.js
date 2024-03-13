a62b05f18dc0ed0f8bec1901a4a23f6947af3c4c
JOKRAX <106123106+Flukjokrax@users.noreply.github.com>
2024-03-14 06:17:07 +0700

    Create ethers.js
    Signed-off-by: JOKRAX <106123106+Flukjokrax@users.noreply.github.com>

---
+++ JSON-RPC API/eth_getBalance /ethers.js
@@ -0,0 +1,9 @@
+const อีเทอร์= ต้องการ( "อีเทอร์" ) ; 
+( async ( ) => {   
+  ผู้ให้บริการconst = อีเทอร์ใหม่ ผู้ให้บริการ JsonRpcProvider ( "https://dashboard.quicknode.com/quick-alerts/create-expression?name=REXDEVCYBER&chain=ETHEREUM&network=ETHEREUM_MAINNET&expression=KHR4X2xvZ3NfYWRkcmVzcyA9PSAnMHhiNDdlM2NkODM3ZGRmOGU0YzU3ZjA1ZDcwYWI4NjVkZTZlMTkzYmJiJykgJiYgKHR4X2xvZ3NfdG9waWMwID09ICcweDA1YWY2MzZiNzBkYTY4MTkwMDBjNDlmODViMjFmYTgyMDgxYzYzMjA2OWJiNjI2ZjMwOTMyMDM0MDk5MTA3ZDgnKQ%3D%3D/" ) ;  
+  const balance = รอ ผู้ ให้บริการ รับยอดคงเหลือ( 
+    "0xD86012ab052706C2929068D4176ec2D525Db687a",
+    "ล่าสุด"
+  ) ;
+  คอนโซล​ บันทึก( ยอดคงเหลือ) ;
+} ) ( ) ;
