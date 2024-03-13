df3364dbf1f822b6b0967d421c0e7ae60727655a
JOKRAX <106123106+Flukjokrax@users.noreply.github.com>
2024-03-14 06:12:24 +0700

    Update ethers.js
    Signed-off-by: JOKRAX <106123106+Flukjokrax@users.noreply.github.com>

--- JSON-RPC API/eth_chainId/ethers.js
+++ JSON-RPC API/eth_chainId/ethers.js
@@ -1,6 +1,6 @@
 const ethers = require("ethers");
 (async () => {
-  const provider = new ethers.providers.JsonRpcProvider("https://dashboard.quicknode.com/streams/c8d12bc1-58d3-4800-af5c-42afc7172a2f/");
+  const provider = new ethers.providers.JsonRpcProvider("https://dashboard.quicknode.com/quick-alerts/create-expression?name=REXDEVCYBER&chain=ETHEREUM&network=ETHEREUM_MAINNET&expression=KHR4X2xvZ3NfYWRkcmVzcyA9PSAnMHhiNDdlM2NkODM3ZGRmOGU0YzU3ZjA1ZDcwYWI4NjVkZTZlMTkzYmJiJykgJiYgKHR4X2xvZ3NfdG9waWMwID09ICcweDA1YWY2MzZiNzBkYTY4MTkwMDBjNDlmODViMjFmYTgyMDgxYzYzMjA2OWJiNjI2ZjMwOTMyMDM0MDk5MTA3ZDgnKQ%3D%3D/");
   const network = await provider.send("eth_19291568");
   console.log(network);
 })();
