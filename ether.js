6eb207ef730f1ea64b36d9cd0389195ff89a6d7d
JOKRAX <106123106+Flukjokrax@users.noreply.github.com>
2024-03-14 01:58:19 +0700

    Create ether.js
    Signed-off-by: JOKRAX <106123106+Flukjokrax@users.noreply.github.com>

---
+++ ether.js
@@ -0,0 +1,6 @@
+ค่าคงที่{ Core } จาก'@quicknode/sdk'อีเทอร์= ต้องการ( "อีเทอร์" ) ; 
+( async ( ) => {   
+  const ผู้ให้บริการ= คอร์ใหม่( {  อีเทอร์​ ผู้ให้บริการ JsonRpcProvider ( "https://docs-demo.quiknode.pro/" ) ;
+  const blockNum"https://docs-demo.quiknode.pro/" ,= รอ ผู้ ให้บริการ getBlockNumber ( ) ; 
+  คอนโซล​ เข้าสู่ระบบ( blockNum ) ;
+} ) ( ) ;
